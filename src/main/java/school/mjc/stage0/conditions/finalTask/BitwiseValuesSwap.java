e0d2dd7943fd49e1e29497f59f4bdb2786a6756b
Baimenovbn <baibn@mail.ru>
2022-12-23 12:31:53 +0600

    feat: finish task

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -1,6 +1,14 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
+//    first = 6 = 0110
+//    second = 9 = 1001
     public void swap(int first, int second) {
+        first ^= second;
+        second = first ^ second;
+        first ^= second;
+
+        System.out.println(first);
+        System.out.println(second);
     }
 }
